ce4aeb87742765356650e3c147e123023f70c7f4
Tdg <zhizunwudivip2000@163.com>
2022-09-22 16:48:42 +0800

    区分请求method

--- src/main/java/com/boot/demo/config/FilterConfig.java
+++ src/main/java/com/boot/demo/config/FilterConfig.java
@@ -15,6 +15,10 @@ public class FilterConfig {
         FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
         registration.setFilter(new CommonFilter());
         registration.addUrlPatterns("/*");
+        //接入 filter 之后，所有访问的 Web URL 就会被自动统计为 Sentinel 的资源，可以针对单个 URL 维度进行流控。
+        // 若希望区分不同 HTTP Method，可以将 HTTP_METHOD_SPECIFY 这个 init parameter 设为 true，
+        // 给每个 URL 资源加上前缀，比如 GET:/foo。  false只是显示 /foo
+        registration.addInitParameter(CommonFilter.HTTP_METHOD_SPECIFY, "true");
         registration.setName("sentinelFilter");
         registration.setOrder(1);
 
--- src/main/java/com/boot/demo/controller/DemoController.java
+++ src/main/java/com/boot/demo/controller/DemoController.java
@@ -1,18 +1,15 @@
 package com.boot.demo.controller;
 
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.fastjson.JSON;
-import com.boot.demo.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
-//import com.boot.demo.stream.function.Teacher;
 
 /**
  * @DATE 2019/11/4 20:33
@@ -20,23 +17,24 @@ import java.util.Map;
  * @Author Tian Daguang
  **/
 @RestController
-@RequestMapping("sentinel/client")
+@RequestMapping("client/")
 @Slf4j
 public class DemoController {
 
-    @Resource
-    private TestService service;
-//
-//    @SentinelResource(value = "sayHello")
-//    public String sayHello(String name) {
-//        return "Hello, " + name;
-//    }
-
-    @GetMapping(path = "send")
-    public String send() throws Exception {
-        log.info("可以用了！！！");
+    @GetMapping(path = "sendGet")
+    public String sendGet() throws Exception {
+        log.info("sendGet可以用了！！！");
         Map<String, Object> mp = new HashMap<>();
         mp.put("code", 100);
+
+        return JSON.toJSONString(mp);
+    }
+
+    @PostMapping(path = "sendPost")
+    public String sendPost() throws Exception {
+        log.info("sendPost可以用了！！！");
+        Map<String, Object> mp = new HashMap<>();
+        mp.put("code", 200);
 
         return JSON.toJSONString(mp);
     }
