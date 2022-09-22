fd42ace22a1a5323c3239a7427c24f05e6d18785
Tdg <zhizunwudivip2000@163.com>
2022-09-22 15:55:49 +0800

    tdg-sentinel-client  1.8.1

--- src/main/java/com/boot/demo/controller/DemoController.java
+++ src/main/java/com/boot/demo/controller/DemoController.java
@@ -1,11 +1,14 @@
 package com.boot.demo.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.fastjson.JSON;
+import com.boot.demo.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +23,14 @@ import java.util.Map;
 @RequestMapping("sentinel/client")
 @Slf4j
 public class DemoController {
+
+    @Resource
+    private TestService service;
+//
+//    @SentinelResource(value = "sayHello")
+//    public String sayHello(String name) {
+//        return "Hello, " + name;
+//    }
 
     @GetMapping(path = "send")
     public String send() throws Exception {
---
+++ src/main/java/com/boot/demo/service/TestService.java
@@ -0,0 +1,12 @@
+package com.boot.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+//    @SentinelResource(value = "sayHello")
+//    public String sayHello(String name) {
+//        return "Hello, " + name;
+//    }
+}
